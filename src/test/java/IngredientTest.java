5b527dc53ef17fb4c455ec1ad516d7a9d921b294
Maxim Demidov <Maxdem01.04.93@yandex.ru>
2023-06-05 23:32:56 +0500

    commit3

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,7 +1,7 @@
-import org.junit.Assert;
 import org.junit.Test;
 import praktikum.Ingredient;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 import static praktikum.IngredientType.SAUCE;
 
 public class IngredientTest {
